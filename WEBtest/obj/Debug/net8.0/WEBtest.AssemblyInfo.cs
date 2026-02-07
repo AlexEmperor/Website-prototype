e69d2a82dc17515163227a2bd8a6cd20f7b2e5a7
Ygrik1308 <93333702+Ygrik1308@users.noreply.github.com>
2026-02-07 16:04:19 +0300

    тп

--- WEBtest/obj/Debug/net8.0/WEBtest.AssemblyInfo.cs
+++ WEBtest/obj/Debug/net8.0/WEBtest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBtest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c912e8282749748525131f19119fc15f037977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBtest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBtest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
