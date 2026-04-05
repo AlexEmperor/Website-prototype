d5a85fb78e770de89bba11d105f260717668cb1f
Ygrik1308 <93333702+Ygrik1308@users.noreply.github.com>
2026-04-05 21:00:34 +0300

    Работает

--- WEBtest.Db/obj/Debug/net8.0/WEBTest.Db.AssemblyInfo.cs
+++ WEBtest.Db/obj/Debug/net8.0/WEBTest.Db.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBtest.Db")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4f351c8af03b6418460be2587768f3fa1337687")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d1defb9a2c691d164fb8ea7ea41f851e79011d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBtest.Db")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBtest.Db")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
